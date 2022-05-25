72dbe5a89fea8d526c4ce579ab513798d5a5c1a0
Francesc Vendrell <xescoo@gmail.com>
2022-05-25 23:45:26 +0200

    Changelog:
    - Added more helper functions
    - Moved headers to a new file
    - Added comments to functions
    - Moved things around

--- tdosctl/tdosctl.c
+++ tdosctl/tdosctl.c
@@ -3,21 +3,12 @@
 #include <ctype.h>
 
 #include <curl/curl.h>
+#include "tdosctl.h"
 
-#define MAXURLLEN 2048
-#define BASE_URL "archive.org/download/Total_DOS_Collection_18_Archive"
 
-enum tdos_LIST_TYPE { Byyear = 1, Alpha };
+/* init curl and download the gamelists */
+CURL* tdos_init(void) {
 
-void tdos_title_to_url(CURL *curl, char url[], char title[], char base_url[]);
-void tdos_find_year_in_title(char year[], char title[]);
-void tdos_get_gamelist(CURL *curl, enum tdos_LIST_TYPE type, char base_url[]); 
-
-char title[] = "Dune II- The Battle for Arrakis v1.07 (En)(It)(Es) (1992)(Virgin Games, Ltd.) [Strategy].zip";
-
-int main() {
-
-    //char url[MAXURLLEN];
     CURL *curl;
 
     curl_global_init(CURL_GLOBAL_ALL);
@@ -25,18 +16,20 @@ int main() {
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
     curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
 
-    //tdos_title_to_url(curl, url, title);
-    tdos_get_gamelist(curl, Alpha, BASE_URL); 
+    // get gamelists
+    tdos_get_gamelist(curl, Alpha, BASEURL);
+    tdos_get_gamelist(curl, Byyear, BASEURL);
 
-    curl_easy_cleanup(curl);
-    curl_global_cleanup();
-
-    return 0;
+    return curl;
 }
 
+
+/* given a game title in the collection, return a URL ready to be curled
+   to download the game.
+*/
 void tdos_title_to_url(CURL *curl, char url[], char title[], char base_url[]) {
 
-    char year[5];
+    char year[5]; // we need an extra byte to end the string with '\0'
     char url_tmp[MAXURLLEN];
     char title_tmp[strlen(title)+5];
 
@@ -46,12 +39,13 @@ void tdos_title_to_url(CURL *curl, char url[], char title[], char base_url[]) {
     sprintf(url_tmp, "https://%s/%s/%s.zip", base_url, "Games/Files", year);
     // add year to title
     sprintf(title_tmp, "%s/%s", year, title);
-    // construct final url
+    // construct final url scaping title_tmp
     sprintf(url, "%s/%s", url_tmp, curl_easy_escape(curl, title_tmp, 0));
 }
 
 
-/* refactor this: too complex */
+/* TODO: refactor this: too complex */
+/* Extract the year from a game title */
 void tdos_find_year_in_title(char year[], char title[]) {
 
     int i = 0;
@@ -76,19 +70,19 @@ void tdos_find_year_in_title(char year[], char title[]) {
     }
 }
 
+
+/* get the gamelist by year or in alphabetic order, and save it into a file */
 void tdos_get_gamelist(CURL *curl, enum tdos_LIST_TYPE type, char base_url[]) {
 
     char *resource;
-    char url[MAXURLLEN];
     char *filename;
-    FILE *file;
+    char url[MAXURLLEN];
 
     switch (type) {
         case (Byyear):
             resource = "Lists/byyear.txt";
             filename = "byyear.txt";
             break;
-        
         case (Alpha):
             resource = "Lists/alpha.txt";
             filename = "alpha.txt";
@@ -96,11 +90,36 @@ void tdos_get_gamelist(CURL *curl, enum tdos_LIST_TYPE type, char base_url[]) {
     }
 
     sprintf(url, "https://%s/%s", base_url, resource);
-    file = fopen(filename, "w");
+    tdos_easy_perform(curl, filename, url);
+}
 
+
+void tdos_get_game(CURL *curl, char title[], char base_url[]) {
+
+    char url[MAXURLLEN];
+    tdos_title_to_url(curl, url, title, base_url);
+    tdos_easy_perform(curl, title, url);
+}
+
+
+void tdos_easy_perform(CURL *curl, char filename[], char url[]) {
+    FILE *file = fopen(filename, "w");
     curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*) file);
-    curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/7.83.0");
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, USERAGENT);
     curl_easy_perform(curl);
+}
+
+
+int main() {
+
+    CURL *curl = tdos_init();
+
+    tdos_get_game(curl, title, BASEURL);
+
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+
+    return 0;
 }
 
