a8a22db1eddbfebdd397248ee3ae89b2ac08e2bc
Francesc Vendrell <xescoo@gmail.com>
2022-05-25 23:49:27 +0200

    Header file for tdosctl

---
+++ tdosctl/tdosctl.h
@@ -0,0 +1,17 @@
+/* tdosctl.h */
+
+#define MAXURLLEN 2048
+#define BASEURL "archive.org/download/Total_DOS_Collection_18_Archive"
+#define USERAGENT "libcurl-agent/7.83.0"
+
+enum tdos_LIST_TYPE { Byyear = 1, Alpha };
+
+CURL* tdos_init(void);
+void tdos_find_year_in_title(char year[], char title[]);
+void tdos_title_to_url(CURL *curl, char url[], char title[], char base_url[]);
+void tdos_get_game(CURL *curl, char title[], char base_url[]);
+void tdos_get_gamelist(CURL *curl, enum tdos_LIST_TYPE type, char base_url[]); 
+void tdos_easy_perform(CURL *curl, char filename[], char url[]);
+
+char title[] = "Dune II- The Battle for Arrakis v1.07 (En)(It)(Es) (1992)(Virgin Games, Ltd.) [Strategy].zip";
+
